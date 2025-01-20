ac827b5b479713a77e9efffd696496f0e024fdf7
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-20 12:15:56 +0100

    modules

--- banking-backend-nest/src/app.module.ts
+++ banking-backend-nest/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { DepositModule } from './deposit/deposit.module';
+import { WithdrawalModule } from './withdrawal/withdrawal.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AccountModule, TransactionModule, DepositModule, WithdrawalModule],
   controllers: [AppController],
   providers: [AppService],
 })
