ba1b19e4f1b586be52faacd0a8b8471ca015017e
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-20 16:53:12 +0100

    other endpoints

--- banking-backend-nest/src/deposit/deposit.controller.ts
+++ banking-backend-nest/src/deposit/deposit.controller.ts
@@ -1,33 +1,17 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { DepositService } from './deposit.service';
 import { CreateDepositDto } from './dto/create-deposit.dto';
-import { UpdateDepositDto } from './dto/update-deposit.dto';
 
 @Controller('deposit')
 export class DepositController {
   constructor(private readonly depositService: DepositService) {}
 
-  @Post()
-  create(@Body() createDepositDto: CreateDepositDto) {
-    return this.depositService.create(createDepositDto);
-  }
-
-  @Get()
+  @Get('getAll')
   findAll() {
     return this.depositService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.depositService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDepositDto: UpdateDepositDto) {
-    return this.depositService.update(+id, updateDepositDto);
-  }
-
-  @Delete(':id')
+  @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.depositService.remove(+id);
   }
